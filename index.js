779f00abc56207bf2a0ab39c54894d1f4608f52f
petercrawford <pcraw1027@gmail.com>
2021-03-16 23:05:54 -0400

    commit tree 7e9562ce70df731e3f29132b2a3f115180fe685f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(activity) {
+    if (activity == undefined) {
+        activity = "roller-skate"
+    }
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity) {
+    if (activity == undefined) {
+        activity = "go to the office"
+    }
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(outParam="*") {
+    return function(inParam="special") {
+        return `You are ${outParam}${inParam}${outParam}!`
+    }
+}
